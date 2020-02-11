20a26d5b1840844002e8a0d0cc2e9ef3ebecf73f
Mahnatse-rgb <56162560+Mahnatse-rgb@users.noreply.github.com>
2020-02-11 10:36:20 +0200

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,3 +1,10 @@
+'''
+Exercise: Draw a square
+
+Write a function, name it square. It takes in an integer and then prints out a square using the hash character.
+
+'''
+
 def square(x):
     x = int(input("Enter the number of rows: "))
     row = "#"*x
