43c5e9edba64e48e7be3fa6c8bbb1fbef928d987
Mahnatse-rgb <56162560+Mahnatse-rgb@users.noreply.github.com>
2020-02-11 10:45:31 +0200

    Update frame_text.py

--- frame_text.py
+++ frame_text.py
@@ -1,3 +1,9 @@
+'''
+Exercise: Frame some text
+
+Write a function that takes a list of strings an prints them, one per line, in a rectangular frame.
+'''
+
 def print_in_a_frame(*words):
     size = max(len(word) for word in words)
     print('*' * (size + 4))
